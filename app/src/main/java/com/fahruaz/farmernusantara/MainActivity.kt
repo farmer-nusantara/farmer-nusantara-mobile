07381ddff07d0de75528731d6b847697fcb587ec
alifmaulanarizqi <alifmaulana312@gmail.com>
2022-05-10 23:07:30 +0700

    add farmland activity

--- app/src/main/java/com/fahruaz/farmernusantara/MainActivity.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/MainActivity.kt
@@ -2,12 +2,39 @@ package com.fahruaz.farmernusantara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.fahruaz.farmernusantara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         supportActionBar?.hide()
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.farmlandFragment,
+            R.id.mapFragment,
+            R.id.profileFragment
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding?.bottomNavigationView?.setupWithNavController(navController)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
 }

33c50555f4a19f9790a5298c77f5d95cb4327bca
alifmaulanarizqi <alifmaulana312@gmail.com>
2022-05-12 12:30:57 +0700

    fix problem after merge

--- app/src/main/java/com/fahruaz/farmernusantara/ui/fragment/farmland/FarmlandFragment.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/ui/fragment/farmland/FarmlandFragment.kt
@@ -11,7 +11,6 @@ import com.fahruaz.farmernusantara.R
 import com.fahruaz.farmernusantara.databinding.FragmentFarmlandBinding
 import com.fahruaz.farmernusantara.ui.DetailDiseaseActivity
 
-
 class FarmlandFragment : Fragment() {
 
     private var binding: FragmentFarmlandBinding? = null

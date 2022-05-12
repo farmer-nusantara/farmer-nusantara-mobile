7a66dac079d04fd9d94120f7f1b836a2b0996bbd
Fahrudin Nasikh Az zuhdu <19523005@students.uii.ac.id>
2022-05-12 13:53:39 +0700

    add edit profil ui

--- app/src/main/java/com/fahruaz/farmernusantara/ui/EditProfileActivity.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/ui/EditProfileActivity.kt
@@ -2,11 +2,24 @@ package com.fahruaz.farmernusantara.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fahruaz.farmernusantara.R
+import com.fahruaz.farmernusantara.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private var binding: ActivityEditProfileBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // toolbar
+        setSupportActionBar(binding?.tbEditProfile)
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.tbEditProfile?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }

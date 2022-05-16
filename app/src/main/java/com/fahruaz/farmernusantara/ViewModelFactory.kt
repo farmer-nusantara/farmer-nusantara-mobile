447fa73cb7c3e54c34d45317dd2fc4d50d66124b
Fahrudin Nasikh Az zuhdu <19523005@students.uii.ac.id>
2022-05-16 13:32:44 +0700

    add register feature

---
+++ app/src/main/java/com/fahruaz/farmernusantara/ViewModelFactory.kt
@@ -0,0 +1,33 @@
+package com.fahruaz.farmernusantara
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.fahruaz.farmernusantara.preferences.UserPreferences
+import com.fahruaz.farmernusantara.viewmodels.RegisterViewModel
+
+class ViewModelFactory(private val pref: UserPreferences, private val context: Context) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+//            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+//                MainViewModel(pref, Injection.provideRepository(context),
+//                    Injection.provideDatabase(context), Injection.provideApiService()) as T
+//            }
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(pref) as T
+            }
+//            modelClass.isAssignableFrom(AddStoryViewModel::class.java) -> {
+//                AddStoryViewModel(pref) as T
+//            }
+//            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+//                LoginViewModel(pref) as T
+//            }
+//            modelClass.isAssignableFrom(MapStoryViewModel::class.java) -> {
+//                MapStoryViewModel(pref) as T
+//            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+}
--- app/src/main/java/com/fahruaz/farmernusantara/ui/RegisterActivity.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/ui/RegisterActivity.kt
@@ -1,18 +1,53 @@
 package com.fahruaz.farmernusantara.ui
 
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.fahruaz.farmernusantara.R
+import com.fahruaz.farmernusantara.ViewModelFactory
+import com.fahruaz.farmernusantara.databinding.ActivityRegisterBinding
+import com.fahruaz.farmernusantara.models.UserModel
+import com.fahruaz.farmernusantara.preferences.UserPreferences
+import com.fahruaz.farmernusantara.viewmodels.RegisterViewModel
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var registerViewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupViewModel()
+
+//        registerViewModel.isLoading.observe(this) {
+//            showLoading(it)
+//        }
+        registerViewModel.toast.observe(this) {
+            showToast(it)
+        }
+
+        action()
         setupView()
+    }
+
+    private fun setupViewModel() {
+        registerViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreferences.getInstance(dataStore), this)
+        )[RegisterViewModel::class.java]
     }
 
     private fun setupView() {
@@ -26,5 +61,51 @@ class RegisterActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun action() {
+        binding.registerButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val phone = binding.phoneEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val passwordConfirm = binding.repeatPasswordEditText.text.toString()
+            when {
+                name.isEmpty() -> {
+                    binding.nameEditTextLayout.error = resources.getString(R.string.empty_name)
+                }
+                email.isEmpty() -> {
+                    binding.emailEditTextLayout.error = resources.getString(R.string.empty_email)
+                }
+                phone.isEmpty() -> {
+                    binding.phoneEditText.error = resources.getString(R.string.phone_email)
+                }
+                password.isEmpty() -> {
+                    binding.passwordEditTextLayout.error = resources.getString(R.string.empty_password)
+                }
+                passwordConfirm.isEmpty() -> {
+                    binding.repeatPasswordEditText.error = resources.getString(R.string.empty_repeat_password)
+                }
+                else -> {
+                    val user = UserModel(email = email, name = name, phone = phone,
+                        password = password, passwordConfirm = passwordConfirm)
+
+                    registerViewModel.registerUser(user)
+                }
+            }
+
+        }
+    }
+
+//    private fun showLoading(isLoading: Boolean) {
+//        if (isLoading) {
+//            binding.progressBar.visibility = View.VISIBLE
+//        } else {
+//            binding.progressBar.visibility = View.GONE
+//        }
+//    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }

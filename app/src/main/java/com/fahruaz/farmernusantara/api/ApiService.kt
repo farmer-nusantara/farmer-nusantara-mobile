c03299f8a045ad66729583975b85d88e39d0f451
Fahrudin Nasikh Az zuhdu <19523005@students.uii.ac.id>
2022-05-16 19:41:19 +0700

    add loginViewModel

--- app/src/main/java/com/fahruaz/farmernusantara/api/ApiService.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.fahruaz.farmernusantara.api
 
 import com.fahruaz.farmernusantara.response.auth.RegisterMessageResponse
+import com.fahruaz.farmernusantara.response.auth.SignInMessageResponse
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -16,4 +17,11 @@ interface ApiService {
         @Field(value = "password") passwordResponse: String,
         @Field(value = "passwordConfirmation") passwordConfirmation: String,
     ): Call<RegisterMessageResponse>
+
+    @FormUrlEncoded
+    @POST("auth/signin")
+    fun loginUser(
+        @Field(value = "email") email: String,
+        @Field(value = "password") password: String
+    ): Call<SignInMessageResponse>
 }
--- app/src/main/java/com/fahruaz/farmernusantara/preferences/UserPreferences.kt
+++ app/src/main/java/com/fahruaz/farmernusantara/preferences/UserPreferences.kt
@@ -5,6 +5,8 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.fahruaz.farmernusantara.models.UserModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class UserPreferences private constructor(private val dataStore: DataStore<Preferences>) {
     suspend fun saveUser(user: UserModel) {
@@ -16,6 +18,28 @@ class UserPreferences private constructor(private val dataStore: DataStore<Prefe
             preferences[PASSWORD_CONFIRM_KEY] = user.passwordConfirm!!
         }
     }
+
+    fun getUser(): Flow<UserModel> {
+        return dataStore.data.map { preferences ->
+            UserModel(
+                preferences[EMAIL_KEY] ?: "",
+                preferences[NAME_KEY] ?: "",
+                preferences[PHONE_KEY] ?: "",
+                preferences[PASSWORD_KEY] ?: ""
+            )
+        }
+    }
+
+    suspend fun login(user: UserModel) {
+        dataStore.edit { preferences ->
+            preferences[EMAIL_KEY] = user.email!!
+            preferences[NAME_KEY] = user.name!!
+            preferences[PHONE_KEY] = user.phone!!
+            preferences[PASSWORD_KEY] = user.password!!
+        }
+    }
+
+
 
     companion object {
         @Volatile
